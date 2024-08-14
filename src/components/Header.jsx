fcd4eb9d8745e046ccc29cf865d874b6439ed2c5
Dipu Sah <sahd7929@gmail.com>
2024-08-14 12:23:31 +0530

    initial setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link, Outlet,NavLink } from 'react-router-dom'
+
+
+export default function Header() {
+  return (
+  <>
+    <header className="bg-gray-800 text-white p-4">
+      <nav className="container mx-auto flex justify-between items-center">
+        <div className="flex-grow lg:flex lg:justify-center space-x-4 lg:space-x-8">
+
+        <NavLink 
+            to="/" 
+            className={({ isActive }) =>`${ isActive ?  "text-blue-400" : "text-white"}  hover:font-bold `}
+          >
+             Home
+        </NavLink>
+
+        <NavLink 
+            to="employeelist"
+            className={({ isActive }) =>`${ isActive ?  "text-blue-400" : "text-white"}  hover:font-bold `}
+          >
+             EmployeeList
+        </NavLink>    
+
+
+      </div>
+
+      <div className="flex space-x-4 lg:space-x-8 ml-auto">
+
+        {/* <Link to="https://www.linkedin.com/in/sahd7929/" target="_blank">
+          <img src={LinkedinLogo} alt='Linkedin Logo'  width={30}/>
+          </Link>
+
+        <Link to="https://github.com/dsah567/" target="_blank">
+          <img src={GithubLogo} alt='Github Logo' width={30} className='bg-slate-50 p-0.5'/>
+        </Link> */}
+
+      </div>
+      </nav>
+    </header>
+
+    <Outlet/>
+  </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from "react-router-dom"
+
+import Header from './components/Header.jsx'
+import Dashboard from './components/Dashboar.jsx'
+import EmployeeList from './components/EmployeeList'
+import Error from './components/Error.jsx'
+
+const route = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Header/>} errorElement={<Error/>}>
+    <Route index element={<Dashboard/>} />
+    <Route path="employeelist" element={<EmployeeList/>} />
+    
+  </Route>
+))
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={route}/>
+  </React.StrictMode>,
+)
